63f40ec88df089123b0ecd71fcf403f4d99663fa
Karol Kozłowski <karol.z.kozlowski@gmail.com>
2024-02-05 20:07:31 +0100

    Add idempotency to backuped objects

--- rocky/upgrade.sh
+++ rocky/upgrade.sh
@@ -5,13 +5,14 @@ ROCKY_RELEASE=9
 REPO_URL="https://download.rockylinux.org/pub/rocky/${ROCKY_RELEASE}/BaseOS/x86_64/os/Packages/r"
 PKG_LIST="/tmp/rocky-${ROCKY_RELEASE}-pkg.txt"
 
+BACKUPS=()
+BACKUPS+=(/usr/share/redhat-logos)
+
 curl -sS -L "${REPO_URL}" | grep href | sed 's/.*href="\(.*\)".*/\1/' > "${PKG_LIST}"
 
 RELEASE_PKG=$(grep rocky-release "${PKG_LIST}" | head -n 1)
 REPOS_PKG=$(grep rocky-repos "${PKG_LIST}" | head -n 1)
 GPG_KEYS_PKG=$(grep rocky-gpg-keys "${PKG_LIST}" | head -n 1)
-
-cat ${PKG_LIST}
 
 dnf install "${REPO_URL}/${RELEASE_PKG}" \
             "${REPO_URL}/${REPOS_PKG} \
@@ -19,9 +20,14 @@ dnf install "${REPO_URL}/${RELEASE_PKG}" \
 
 rm "${PKG_LIST}"
 
+for item in ${BACKUPS[*]}; do
+    if [ -e "${item}" ]; then
+      mv "${item}" "${item}.bak"
+    fi
+done
+
 mv /usr/share/redhat-logos /usr/share/redhat-logos.bak
 
 dnf -y --releasever=${ROCKY_RELEASE} --allowerasing --setopt=deltarpm=false distro-sync
 
 rpm --rebuilddb
-
